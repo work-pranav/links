57497e7e0df0dc6a67aa255792c9382455a13beb
work-pranav <155452627+work-pranav@users.noreply.github.com>
2024-07-13 05:15:01 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,22 @@
 export const Name = "Pranav Goel";
 
-export const Description = "I am PG"
+export const Description = "A 19-year-old designer currently pursuing a bachelor's degree in Design at IIT Hyderabad. Specializing in 2D, 3D, AR, and game design, he has a passion for exploring the realms of art, design, and everything in between."
 
 export const Links = [
     {
-        title: "My Portfolio | CV",
+        title: "My Portfolio",
         link: "https://www.canva.com/design/DAF9auw5psI/chJNQIVgPqDCk0iJbI5Tvw/view"
     },
     {
         title: "Instagram",
         link: "https://www.instagram.com/work.pranav/"
+    },
+    {
+        title: "LinkedIn",
+        link: "https://www.linkedin.com/in/work-pranavgoel/"
+    },
+    {
+        title: "Contact me",
+        link: "mailto:work.pranavgoel@gmail.com"
     },
 ]
