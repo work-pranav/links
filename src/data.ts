a3dc8d9e520ae8cc32a9d0de73e4bcc65c33f5db
work-pranav <155452627+work-pranav@users.noreply.github.com>
2024-07-26 05:07:00 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -5,11 +5,7 @@ export const Description = ""
 export const Links = [
     {
         title: "My Portfolio",
-        link: "I am a Bachelor of Design (B.Des) student at IIT Hyderabad, passionate about exploring and pushing the boundaries of design. As a creator deeply invested in my learning journey, I have developed expertise in 2D design, 3D design, AR, VR, VFX, game design, and more.
-
-    My professional journey began as an animator, with a strong focus on AR experimentation. I also served as the Technical (Design) Head of "The Commerce Forum," one of our college's most renowned forums. This role was instrumental in shaping my identity, as it involved constant interaction with people, fostering incredible bonds.
-    
-    Over the past five years, I have worked professionally, solving problems and deepening my love for design. Moving forward, I aspire to create designs that inspire and positively impact society."
+        link: "I am a Bachelor of Design (B.Des) student at IIT Hyderabad, passionate about exploring and pushing the boundaries of design. As a creator deeply invested in my learning journey, I have developed expertise in 2D design, 3D design, AR, VR, VFX, game design, and more. My professional journey began as an animator, with a strong focus on AR experimentation. I also served as the Technical (Design) Head of "The Commerce Forum," one of our college's most renowned forums. This role was instrumental in shaping my identity, as it involved constant interaction with people, fostering incredible bondsOver the past five years, I have worked professionally, solving problems and deepening my love for design. Moving forward, I aspire to create designs that inspire and positively impact society."
     },
     {
         title: "Instagram",
