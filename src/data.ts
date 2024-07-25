44427aff0e19d86c1881303d0fa03a41173e5850
work-pranav <155452627+work-pranav@users.noreply.github.com>
2024-07-26 05:00:47 +0530

    main-text

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,15 @@
 export const Name = "Pranav Goel";
 
-export const Description = "A 19-year-old designer currently pursuing a bachelor's degree in Design at IIT Hyderabad. Specializing in 2D, 3D, AR, and game design, he has a passion for exploring the realms of art, design, and everything in between."
+export const Description = ""
 
 export const Links = [
     {
         title: "My Portfolio",
-        link: "https://www.canva.com/design/DAF9auw5psI/chJNQIVgPqDCk0iJbI5Tvw/view"
+        link: "I am a Bachelor of Design (B.Des) student at IIT Hyderabad, passionate about exploring and pushing the boundaries of design. As a creator deeply invested in my learning journey, I have developed expertise in 2D design, 3D design, AR, VR, VFX, game design, and more.
+
+    My professional journey began as an animator, with a strong focus on AR experimentation. I also served as the Technical (Design) Head of "The Commerce Forum," one of our college's most renowned forums. This role was instrumental in shaping my identity, as it involved constant interaction with people, fostering incredible bonds.
+    
+    Over the past five years, I have worked professionally, solving problems and deepening my love for design. Moving forward, I aspire to create designs that inspire and positively impact society."
     },
     {
         title: "Instagram",
