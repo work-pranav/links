3b4f6adffbbf90681402b13402bc3e587890a74f
work-pranav <155452627+work-pranav@users.noreply.github.com>
2026-07-03 02:20:49 +0530

    texts updated

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,9 @@ function App() {
         <p className="font-medium absolute p-1.5 left-0 right-0 font-bold mx-auto w-32 text-center bottom-0.5 bg-white rounded-md border-2 border-[#a369f4] shadow-custom-purple-short text-lg">{Name}</p>
         <img src={logo} alt="pranav-stuff-logo.svg" className="absolute w-14 right-0 top-0" />
       </div>
-      <p className="-my-6 mx-3 p-3 text-center font-bold pt-8 rounded-3xl border-2 border-[#a369f4] shadow-custom-purple -bottom-[8.5rem] -z-10">{Description}</p>
-      <div className="my-6">
-        <div className="p-5 px-10 flex flex-col w-full gap-3">
+      <p className="font-normal -my-6 mx-3 p-3 text-center font-bold pt-8 rounded-3xl border-2 border-[#a369f4] shadow-custom-purple -bottom-[8.5rem] -z-10">I'm a multidisciplinary designer specializing in 2D, 3D, AR, VFX, and interactive experiences, with <span className="font-medium"> 6+ years of professional experience.</span> My work has contributed to projects for brands including <span className="font-medium">Pepsi, Bacardi, UCB, Patrón, and others,</span> driven by a passion for creating meaningful experiences that leave a lasting impact.</p>
+      <div className="my-6"> 
+        <div className="font-medium p-5 px-10 flex flex-col w-full gap-3">
           {Links.map(({title, link}, idx) => <LinkComponent content={title} btnlink={link} key={idx} />)}
         </div>
       </div>
